b90f76157c117b6abede27dd75c3dfe201929754
Christina Schmidt <chschmidt@geomar.de>
2023-08-30 10:07:33 +1000

    ran with 1/40th output

--- SWMT_calculation.sh
+++ SWMT_calculation.sh
@@ -3,7 +3,7 @@
 #PBS -l ncpus=48
 #PBS -l mem=180GB
 #PBS -q normal
-#PBS -l walltime=0:40:00
+#PBS -l walltime=2:00:00
 #PBS -l storage="gdata/hh5+gdata/ik11+gdata/v45+gdata/e14+gdata/cj50+scratch/v45+scratch/x77"
 #PBS -l wd
 #PBS -o calculation_SWMT.out
--- run_SWMT_calculation.sh
+++ run_SWMT_calculation.sh
@@ -2,6 +2,6 @@
 
 ## loop over count, submit job to gadi with count that gets communicated to python
 
-for y in {1998..2002}; do
-   qsub -v year=$y,expt='panant-005-zstar-ACCESSyr2',expt_name='panan_005deg_jra55_ryf' SWMT_calculation.sh
+for y in {1991..1999}; do
+   qsub -v year=$y,expt='panant-0025-zstar-ACCESSyr2',expt_name='panan_0025deg_jra55_ryf' SWMT_calculation.sh
 done
