a62c02a354a294167348485dd96cce99c1e0e527
Christina Schmidt <chschmidt@geomar.de>
2023-09-27 15:13:19 +1000

    adjusted years

--- run_timeseries_calculation.sh
+++ run_timeseries_calculation.sh
@@ -2,10 +2,10 @@
 
 ## loop over count, submit job to gadi with count that gets communicated to python
 
-# for y in {1991..2009..10}; do
-#    qsub -v year=$y,expt='panan-01-test-oldparams',expt_name='panan_01deg_jra55_ryf' Timeseries_calculation.sh
-# done
+for y in {1991..2009..10}; do
+   qsub -v year=$y,expt='panant-01-zstar-ACCESSyr2',expt_name='panan_01deg_jra55_ryf' Timeseries_calculation.sh
+done
 
-for y in {1998..2005}; do
+for y in {1991..2010}; do
     qsub -v year=$y,expt='panant-005-zstar-ACCESSyr2',expt_name='panan_005deg_jra55_ryf' Timeseries_calculation.sh
 done
