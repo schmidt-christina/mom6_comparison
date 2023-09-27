df959278dccd4183f4b12b2b1476187be622139c
Christina Schmidt <chschmidt@geomar.de>
2023-09-28 08:11:22 +1000

    updated years and experiment names

--- run_SWMT_calculation.sh
+++ run_SWMT_calculation.sh
@@ -2,14 +2,19 @@
 
 ## loop over count, submit job to gadi with count that gets communicated to python
 
-for y in {1991..2010}; do
+for y in {1991..2005}; do
    qsub -v year=$y,expt='panant-01-zstar-ACCESSyr2',expt_name='panan_01deg_jra55_ryf' SWMT_calculation.sh
+done
+
+for y in {2006..2010}; do
+   qsub -v year=$y,expt='panant-01-zstar-ACCESSyr2_first_run',expt_name='panan_01deg_jra55_ryf' SWMT_calculation.sh
 done
 
 
 for y in {1991..2010}; do
    qsub -v year=$y,expt='panant-005-zstar-ACCESSyr2',expt_name='panan_005deg_jra55_ryf' SWMT_calculation.sh
 done
+
 
 for y in {1991..1999}; do
    qsub -v year=$y,expt='panant-0025-zstar-ACCESSyr2',expt_name='panan_0025deg_jra55_ryf' SWMT_calculation.sh
